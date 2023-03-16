d9dcaca6fa24beeed733bc65cbc86922119591bb
KareemMarzouk <bkmarzouk@outlook.com>
2023-03-16 17:32:24 +0000

    line parser for rsync output

---
+++ synchro/utils/proc_rsync_out.py
@@ -0,0 +1,67 @@
+# Dictionaries based on description here:
+# https://stackoverflow.com/a/36851784/21297098
+
+update_dict = {
+    ".": "Item is not being updated..............................",
+    "<": "New item to be sent to the remote host.................",
+    ">": "New item to be received by the local host..............",
+    "c": "Local change to / creation of item (e.g. dir, link)....",
+    "h": "Hard link detected.....................................",
+    "*": "Itemized output contains a message.....................",
+}
+
+type_dict = {
+    "f": "file.......",
+    "d": "directory..",
+    "L": "symlink....",
+    "D": "device.....",
+    "S": "special....",
+}
+
+
+def parse_line(line):
+    """
+    Parse line from rsync itemized output,
+    to determine whether transfer needs to take place
+    e.g.
+
+    >f+++++++++ path/to/new/file
+    .f...p..... path/to/old/file/with/diff/permissions
+
+    This latter case is relatively unimportant
+    if we have transferred with changing permissions!
+
+    We don't want to tar a massive directly and send
+    this just to redo the permission change upon receipt.
+    """
+
+    bit_positions, filename = line.split(" ")
+
+    update_bit, type_bit, *other_bits = bit_positions
+
+    update_desc = update_dict[update_bit]
+    type_desc = type_dict[type_bit]
+
+    output_message = f"{update_desc} : {type_desc} : {filename}"
+
+    # TODO: Formulate better methods from user end to make these decisions.
+    #  Currently just ignores . bits
+    return False if update_bit == "." else True, output_message
+
+
+if __name__ == "__main__":
+    test_data = [
+        ".d..tp..... ./",
+        ".f...p..... changes.py",
+        ".f...p..... content",
+        ".f...p..... run.sh",
+        ".d...p..... new_dir/",
+        ".f...p..... new_dir/something.py",
+        ".d...p..... some_dir/",
+        ".f...p..... some_dir/mega_file",
+        ".f...p..... some_dir/test_file.txt",
+    ]
+
+    for dat in test_data:
+        value, msg = parse_line(dat)
+        print(msg)
