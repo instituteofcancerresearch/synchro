3c5e5b9a7e830210c4cb675de50be49a090b6a21
KareemMarzouk <bkmarzouk@outlook.com>
2023-03-22 14:26:42 +0000

    update template conf file for testing to include email pars

--- tests/utils/utils.py
+++ tests/utils/utils.py
@@ -9,6 +9,11 @@ def create_conf_file(
     tar="y",
     untar="y",
     create_dest="y",
+    email_address="person@place.com",
+    email_on_start="n",
+    email_on_end="n",
+    transfer_ongoing_file_exists=False,
+    persistent_log_file=False,
 ):
     conf_file = source_dir / filename
     with open(conf_file, "w") as f:
@@ -17,6 +22,9 @@ def create_conf_file(
         f.write(f"tar = {tar}\n")
         f.write(f"untar = {untar}\n")
         f.write(f"create_dest = {create_dest}\n")
+        f.write(f"email_address = {email_address}\n")
+        f.write(f"email_on_start = {email_on_start}\n")
+        f.write(f"email_on_end = {email_on_end}\n")
 
         if ready_file is not None:
             f.write(f"transfer_ready_file = {ready_file}\n")
