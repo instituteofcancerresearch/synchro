ca1ce283c0a226d656170b56d5aa3bc48e7ef5aa
KareemMarzouk <bkmarzouk@outlook.com>
2023-03-23 15:03:33 +0000

    update tests to reflect new email structure

--- tests/test_unit/test_utils/test_emails.py
+++ tests/test_unit/test_utils/test_emails.py
@@ -12,14 +12,15 @@ class TestEmailClass:
     email_null = Email.null()
 
     def test_no_attachment(self):
-        cmd = self.email_no_attachment.get_command()
+        echo_cmd, mail_cmd = self.email_no_attachment.get_command()
 
-        assert cmd[0] == "echo"
-        assert cmd[1] == "|"
-        assert cmd[2] == "mailx"
-        assert cmd[3] == "-s"
-        assert cmd[4] == self.subject
-        assert cmd[5] == self.address
+        assert echo_cmd[0] == "echo"
+        assert echo_cmd[1] == ""
+
+        assert mail_cmd[0] == "mailx"
+        assert mail_cmd[1] == "-s"
+        assert mail_cmd[2] == self.subject
+        assert mail_cmd[3] == self.address
 
     def test_inc_attachement(self):
         cat_cmd, mail_cmd = self.email_inc_attachment.get_command()
